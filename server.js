61e7d5ea54c452a3e14d7c81b3ac64f4aee11622
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2020-07-30 12:44:08 +0530

    Add @currentUsers command

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+//send current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if(typeof(info) === 'undefined') {
+		return;
+	}
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if(info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment.valueOf()
+	});
+}
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 	
@@ -39,8 +62,13 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 		
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if(message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		}
+		else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 		//socket.broadcast.emit('message', message); //sends to everybody but user, io.emit send to everybody
 	});
 
