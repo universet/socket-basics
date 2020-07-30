2976e1ea88d6920f682e2e3719d13591736074bf
Tejaswini Jadhav <tejujadhav7758@gmail.com>
2020-07-30 09:54:19 +0530

    Add name support

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app); //tells the node to start a new sever and to use this express app as bioler plate
 var io = require('socket.io')(http);
-var moment = require('moment');
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,7 +12,7 @@ io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 	
 	socket.on('message', function (message) {
-		console.log('Message recieved: ' + message.text);
+		console.log('Message received: ' + message.text);
 		
 		message.timestamp = moment().valueOf();
 		io.emit('message', message);
@@ -19,6 +20,7 @@ io.on('connection', function (socket) {
 	});
 
 	socket.emit('message', {
+		name: 'System',
 		text: 'Welcome to the chat application!',
 		timestamp: moment().valueOf()
 	});
